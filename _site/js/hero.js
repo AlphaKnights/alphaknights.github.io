1b03c84d552dd0307009692b9ce8a8d4e9908f03
TJ Horner <me@tjhorner.com>
2017-01-26 21:11:16 -0800

    pre

---
+++ _site/js/hero.js
@@ -0,0 +1,6 @@
+(function(){
+  $(".social-presence-video").click(function(){
+    $(".video-standby").fadeIn();
+    $(".video").attr("src", "https://www.youtube.com/embed/C0K3IzrhW7s/?autoplay=1");
+  })
+}())
