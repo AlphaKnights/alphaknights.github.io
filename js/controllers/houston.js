e705143b7f4cd33c639010873543023c1f2cbbdd
TJ Horner <me@tjhorner.com>
2017-04-21 16:29:52 -0500

    asdasd

--- js/controllers/houston.js
+++ js/controllers/houston.js
@@ -3,8 +3,22 @@ siteApp.controller('houstonController', function($scope, $http){
 
   function getMatches(){
     $.getJSON(API_HOST + "/matches").then(res => {
-      $scope.matches = res.sort((a, b) => {
+      $scope.matches = [ ]
+
+      res = res.sort((a, b) => {
         return a.match_number - b.match_number
+      })
+
+      res.forEach(match => {
+        var akAlliance = match.alliances.blue.teams.indexOf("frc6695") !== -1 ? "blue" : "red"
+
+        $scope.matches.push({
+          match_number: match.match_number,
+          score_breakdown: match.score_breakdown,
+          akAlliance,
+          alliances: match.alliances,
+          akWin: match.score_breakdown ? (match.alliances[akAlliance].score > match.alliances[akAlliance === "blue" ? "red" : "blue"].score ? true : false) : false
+        })
       })
 
       $scope.$apply()
